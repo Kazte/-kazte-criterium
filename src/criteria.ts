c61c11e46337c81f401e28f77f2eddf84054b604
Kazte <castellittofranco1@gmail.com>
2025-01-24 11:07:53 -0300

    fix: update to abtract impl

--- src/criteria.ts
+++ src/criteria.ts
@@ -1,3 +1,9 @@
-export interface Criteria<T> {
-  meetCriteria(items: T[]): T[];
+export abstract class Criteria<T> {
+  abstract meetCriteria(items: T[]): T[];
+
+  static create<T>(predicate: (item: T) => boolean): Criteria<T> {
+    return {
+      meetCriteria: (items: T[]) => items.filter(predicate)
+    };
+  }
 }
